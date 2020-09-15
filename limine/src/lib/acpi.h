6a3ac030903e1b638c0f699d4d7dd5dd95f26c88
Soumitra Shewale <soumitra.shewale.dev@gmail.com>
2020-09-15 11:02:53 +0530

    idk how submodules work

--- limine/src/lib/acpi.h
+++
@@ -1,6 +0,0 @@
-#ifndef __LIB__ACPI_H__
-#define __LIB__ACPI_H__
-
-void *get_rsdp(void);
-
-#endif
--- limine/src/protos/stivale.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PROTOS__STIVALE_H__
-#define __PROTOS__STIVALE_H__
-
-#include <fs/file.h>
-
-void stivale_load(char *cmdline, int boot_drive);
-
-#endif
