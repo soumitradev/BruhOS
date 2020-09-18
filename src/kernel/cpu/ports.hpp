6ca6791bdee540f3a877564ab52c27157a160ff4
Soumitra Shewale <soumitra.shewale.dev@gmail.com>
2020-09-19 00:12:28 +0530

    Add Device IO Port interface code
    I also removed the intel masm flag btw :flushed:

---
+++ src/kernel/cpu/ports.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <stdint.h>
+
+uint8_t port_byte_in(unsigned short port);
+void port_byte_out(unsigned short port, unsigned char data);
+uint16_t port_word_in(unsigned short port);
+void port_word_out(unsigned short port, unsigned short data);
